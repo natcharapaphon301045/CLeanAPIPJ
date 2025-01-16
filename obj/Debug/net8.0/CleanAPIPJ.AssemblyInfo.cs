9e75d20599159fd042b48f721af92999dd9b5734
natcharapaphon <6431503019@lamduan.mfu.ac.th>
2025-01-16 17:46:19 +0700

    ล่าสุด

--- obj/Debug/net8.0/CleanAPIPJ.AssemblyInfo.cs
+++ obj/Debug/net8.0/CleanAPIPJ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanAPIPJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30229c2138edb0353109232534670267451d4e12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+071b0f12188ab31ceebea164ee4afff2d7aede34")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanAPIPJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanAPIPJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
